9f94e56ab988ad8acf708426a054a9e3b6f7698b
cjdeclaro <cjdeclaro16@gmail.com>
2025-11-18 22:53:46 +0800

    M01 BSIT 2-1

---
+++ pupstc/lessons/2526S1/data/BSIT 2-1.js
@@ -0,0 +1,257 @@
+var BSIT2120252026S1 = [
+  {
+    "lastName": "ABARQUEZ",
+    "firstName": "LAURENCE PAUL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "ABELLA",
+    "firstName": "CHRIZYL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "ABIERA",
+    "firstName": "KRYSTEL MAE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "ABIÑO",
+    "firstName": "JENNIFER KYLIE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "AMILER",
+    "firstName": "JAY PATRICK",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "AMOSCO",
+    "firstName": "BETHANY ZYREEN",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "ATINON",
+    "firstName": "JHODY",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "BELARMINO",
+    "firstName": "CARL JUSTIN",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "BOTE",
+    "firstName": "JOSHUA MIGUEL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "CAIDIC",
+    "firstName": "GLEN SIDNEY",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "CUTARAN",
+    "firstName": "VERGEL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "DACOLIAT",
+    "firstName": "PAUL JOSHUA",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "DE LA PEÑA",
+    "firstName": "IVAN",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "DOMINO",
+    "firstName": "DENZEL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "EBORA",
+    "firstName": "SEBASTIAN LUIS",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "ESTAÑOL",
+    "firstName": "JEXEQUIEL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "EVANGELISTA",
+    "firstName": "ARISH JAN",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "EXCONDE",
+    "firstName": "MARC JUSTINE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "FORTIN",
+    "firstName": "MYCA CIELO",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "GONZAGA",
+    "firstName": "GLEN GABRIEL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "GUEVARRA",
+    "firstName": "EDWARD",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "ILAO",
+    "firstName": "JEN MAE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "LAT",
+    "firstName": "DONNA MAE DE JUSAY",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "LINATOC",
+    "firstName": "KIMBERLY",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "LOMA",
+    "firstName": "ALDRICH SAM",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "LOMIBAO",
+    "firstName": "CHASTE ANTHONY",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "LUZANO",
+    "firstName": "MARK ANTHONY",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "MAGBOO",
+    "firstName": "MARY JOSEPHINE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "MAGPAYO",
+    "firstName": "SHEMAIAH EZRA",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "MALABANAN",
+    "firstName": "RYAN POL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "MALAIBA",
+    "firstName": "JUSTINE LUIGE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "MANZO",
+    "firstName": "JOSE MIGUEL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "MERCADO",
+    "firstName": "KAILE ARKIN",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "MONCAYO",
+    "firstName": "AERYLL REIGN",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "ONDO",
+    "firstName": "LANCE GABRIEL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "PADUHILAO",
+    "firstName": "NATALIE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "PALOMO",
+    "firstName": "LIAM MATHEW",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "PATDU",
+    "firstName": "PATRIO GABRIEL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "PERCAL",
+    "firstName": "GABRIEL",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "PEÑA",
+    "firstName": "CARL ANTHONY",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "REYES",
+    "firstName": "LANCE CEDRICK",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "RINGOR",
+    "firstName": "RANS CLEMEN EMDEE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "RIVERA",
+    "firstName": "CHARLIE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "ROJO",
+    "firstName": "CHESTER CAREY",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "ROYOL",
+    "firstName": "MARY JANE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "SAN PEDRO",
+    "firstName": "ARYEN JAEF",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "SARICAL",
+    "firstName": "JASON LANCE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "TACHIBANA",
+    "firstName": "SEITO",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "TALAS",
+    "firstName": "TRISHA MAE",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "TANGLAO",
+    "firstName": "WAYNE SAILA",
+    "section": "BSIT 2-1"
+  },
+  {
+    "lastName": "TRIA",
+    "firstName": "MARK JUSTIN",
+    "section": "BSIT 2-1"
+  }
+]
